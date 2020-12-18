59533b2f7320f9fb26df8a2acbe346ef57be30dc
JunhwanKang <wnsghks1017@naver.com>
2020-12-18 23:48:23 +0900

    백준 11654

---
+++ baekj11654.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekj11654 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		char c = str.charAt(0);
+		int num = c;
+		System.out.println(num);
+	}
+}
